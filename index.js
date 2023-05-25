19ae9f165da459f30ea3d24f6c619d48ee4664bd
rafaelnaranjo <rrnp.naranjo@gmail.com>
2023-05-25 16:19:15 -0500

    configure cors

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler } = require("./middleware/error.handler")
 
 const app = express();
 
 app.use(express.json());
-
+const whitelist = ['http://localhost:8080', 'http://localhost:3000']
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin)) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+app.use(cors(corsOptions));
 routerApi(app);
 
 app.use(logErrors);
